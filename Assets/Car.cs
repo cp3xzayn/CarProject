8e92aac6dd4bf39235362e331589294223833444
cp3xzayn <cp3xzayn@gmail.com>
2021-04-02 21:11:44 +0900

    タイヤが動くようにした。車のアセットを変更し、実装した。

--- Assets/Car.cs
+++ Assets/Car.cs
@@ -9,9 +9,33 @@ public class Car : MonoBehaviour
     [SerializeField] float maxMotorTorque = 1000;
     [SerializeField] float maxSteeringAngle = 40;
 
+    [SerializeField] WheelCollider wheelFL;
+    [SerializeField] WheelCollider wheelFR;
+    [SerializeField] WheelCollider wheelBL;
+    [SerializeField] WheelCollider wheelBR;
+
+    [SerializeField] Transform wheelFLTrans;
+    [SerializeField] Transform wheelFRTrans;
+    [SerializeField] Transform wheelBLTrans;
+    [SerializeField] Transform wheelBRTrans;
+
+    void Update()
+    {
+        //wheelcolliderの回転速度に合わせてタイヤモデルを回転させる
+        wheelFLTrans.Rotate(wheelFL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        wheelFRTrans.Rotate(wheelFR.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        wheelBLTrans.Rotate(wheelBL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        wheelBRTrans.Rotate(wheelBR.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+
+        //wheelcolliderの角度に合わせてタイヤモデルを回転する（フロントのみ）
+        wheelFLTrans.localEulerAngles = new Vector3(wheelFLTrans.localEulerAngles.x, wheelFL.steerAngle - wheelFLTrans.localEulerAngles.z, wheelFLTrans.localEulerAngles.z);
+        wheelFRTrans.localEulerAngles = new Vector3(wheelFRTrans.localEulerAngles.x, wheelFR.steerAngle - wheelFRTrans.localEulerAngles.z, wheelFRTrans.localEulerAngles.z);
+
+    }
+
     public void FixedUpdate()
     {
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float motor = -maxMotorTorque * Input.GetAxis("Vertical");
         float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
         foreach (AxleInfo axleInfo in axleInfos)
         {
