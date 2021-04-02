90c8b94099de32d4a72da5ce0cffe1d545be8486
cp3xzayn <cp3xzayn@gmail.com>
2021-04-02 20:22:18 +0900

    車のアセットを追加し実装した。

---
+++ Assets/Car.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Car : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    [SerializeField] float maxMotorTorque = 1000;
+    [SerializeField] float maxSteeringAngle = 40;
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+}
+
+[Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; //駆動輪か?
+    public bool steering; //ハンドル操作をしたときに角度が変わるか？
+}
